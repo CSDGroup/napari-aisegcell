2632a554a1a281e23d89ac912b0dc72c408b55bc
dsethz <dschirethz@gmail.com>
2023-11-09 16:32:17 +0100

    adapt aisegcell import and remove napari-hub link for now

--- src/napari_aisegcell/__init__.py
+++ src/napari_aisegcell/__init__.py
@@ -1,5 +1,4 @@
-__version__ = "0.0.1"
-
+from ._version import version as __version__
 from ._sample_data import make_sample_data
 from ._widget_batch_mode import make_batch_mode_widget
 from ._widget_layer_mode import make_layer_mode_widget
